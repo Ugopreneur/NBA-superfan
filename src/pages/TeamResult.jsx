824f587e8d9bded9d2af7b14e5dbc746c8ceda24
mister-stan <lucianstan87@yahoo.com>
2023-03-22 21:58:10 +0000

    adds the displayed data into the page

--- src/pages/TeamResult.jsx
+++ src/pages/TeamResult.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
 const TeamResult = (props) => {
-  return (
-    <div>
-    in team result {props.details.name}
-    </div>
+  return ( 
+     <div className="container" style={{ border:"1px solid red"  }}>
+      
+      {/* display the team's name and city in an 'h2' element */}
+      <h2 style={{ display:"flex", justifyContent:"center" }}>
+        {props.details.name} 
+      </h2>
+      <div style={{display:"flex", justifyContent:"center"}}>
+        <img src={props.details.logo} alt={`${props.details.name} logo`} style={{ width: '200px' }} />
+        </div>
+        <h3 style={{ display:"flex", justifyContent:"center" }}>
+        {props.details.nickname} 
+      </h3>
+   </div>
   )
 }
 
