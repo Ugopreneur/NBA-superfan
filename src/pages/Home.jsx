c6a19388fa6b6adae7b4f5e17265f85cf49ee95a
ugopreneur <ugnwokoro@yahoo.com>
2023-03-22 16:52:35 +0000

    created ternanry operator for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,31 +9,32 @@ const Home = () => {
   return (
     // return the jsx for the home page
     <div className="container">
-      {/* apply inline styles to tye img element */ }
-      <img
-        src={Hero}
-        style={{ width: "100%", height: "100vh", objectFit: "cover", opacity: "0.85"}}
-        alt="logo"
-      />
-      <div
-        style={{
-          position: "absolute",
-          top: "84%",
-          left: "0",
-          right: "0",
-          transform: "translateY(-50%)",
-        }}
-      >
-        {/* insert the search bar component */} 
-        <SearchBar setResult = {setTeamDetails} />
-        {teamDetails?(
+      {teamDetails?(
         <TeamResult details={teamDetails.response[0]} />
         
         ) : (
-        <div>no teamDeatails yet</div>
+        <div>{/* apply inline styles to tye img element */ }
+        <img
+          src={Hero}
+          style={{ width: "100%", height: "100vh", objectFit: "cover", opacity: "0.85"}}
+          alt="logo"
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: "84%",
+            left: "0",
+            right: "0",
+            transform: "translateY(-50%)",
+          }}
+        >
+          {/* insert the search bar component */} 
+          <SearchBar setResult = {setTeamDetails} />
+          
+        </div></div>
       
       )}
-      </div>
+      
     </div>
   );
 };
