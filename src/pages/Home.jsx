ee75b17814c0bad1c6e410ed2bb7a4cab7c742c3
ugopreneur <ugnwokoro@yahoo.com>
2023-03-22 18:00:23 +0000

    created button to go back home from results page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,35 +6,53 @@ import TeamResult from "./TeamResult";
 const Home = () => {
   const [teamDetails, setTeamDetails] = useState();
   console.log(`Yes we have got some`, teamDetails);
+
+  const handleClick = () => {
+    setTeamDetails("")
+  };
+
   return (
-    // return the jsx for the home page
     <div className="container">
-      {teamDetails?(
-        <TeamResult details={teamDetails.response[0]} />
-        
-        ) : (
-        <div>{/* apply inline styles to tye img element */ }
-        <img
-          src={Hero}
-          style={{ width: "100%", height: "100vh", objectFit: "cover", opacity: "0.85"}}
-          alt="logo"
-        />
-        <div
-          style={{
-            position: "absolute",
-            top: "84%",
-            left: "0",
-            right: "0",
-            transform: "translateY(-50%)",
-          }}
-        >
-          {/* insert the search bar component */} 
-          <SearchBar setResult = {setTeamDetails} />
-          
-        </div></div>
-      
+      {/* this is a ternanry function for the homepage that displays the hero and search bar by default
+      but displays the team results when the search bar is clicked */}
+      {teamDetails ? (
+        <div>
+          <div>
+            <TeamResult details={teamDetails.response[0]} />
+          </div>
+          <div>
+            <button type="button" onClick={handleClick}>
+              Go Back
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          {/* apply inline styles to the img element */}
+          <img
+            src={Hero}
+            style={{
+              width: "100%",
+              height: "100vh",
+              objectFit: "cover",
+              opacity: "0.85",
+            }}
+            alt="logo"
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "84%",
+              left: "0",
+              right: "0",
+              transform: "translateY(-50%)",
+            }}
+          >
+            {/* insert the search bar component */}
+            <SearchBar setResult={setTeamDetails} />
+          </div>
+        </div>
       )}
-      
     </div>
   );
 };
