98e0d17691d19c3b0f39d5b21bd239b33b9ee711
mister-stan <lucianstan87@yahoo.com>
2023-03-23 04:27:17 +0000

    add the youtube cards layout

--- src/components/TeamHighlights.jsx
+++ src/components/TeamHighlights.jsx
@@ -10,13 +10,14 @@ const TeamHighlights = (props) => {
   useEffect(() => {
     const fetchYoutubeData = async () => {
       const options = {
-        method: 'GET',
-        url: 'https://youtube138.p.rapidapi.com/search/',
-        params: {q: props.details.name, hl: 'en', gl: 'US'},
+        method: "GET",
+        url: "https://youtube138.p.rapidapi.com/search/",
+        params: { q: props.details.name, hl: "en", gl: "US" },
         headers: {
-          'X-RapidAPI-Key': 'd73fe9001amshc84a879808281a3p118f3ejsnc92bedc355cd',
-          'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-        }
+          "X-RapidAPI-Key":
+            "d73fe9001amshc84a879808281a3p118f3ejsnc92bedc355cd",
+          "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+        },
       };
 
       try {
@@ -35,34 +36,44 @@ const TeamHighlights = (props) => {
     return <div>Loading...</div>;
   }
 
-  return (
-    <div>
-      <div className="col-md-6">
-        <div
-          className="card text-center mb-5"
-          style={{
-            borderRadius: "2px",
-            background: "#C0C0C0",
-            boxShadow: "rgba(128, 128, 128, 0.5) 1px 1px 1px 1px",
-          }}
-        >
-          <div className="card-img-top embed-responsive embed-responsive-16by9">
-            <iframe
-              title={youtubeResponse.contents[0].video.title}
-              className="embed-responsive-item"
-              src={`https://www.youtube.com/embed/${youtubeResponse.contents[0].video.videoId}`}
-              allowFullScreen
-              width={"100%"}
-            ></iframe>
-          </div>
-          <div className="card-body">
-            {/* change the string  */}
-            <h4 className="card-title">{youtubeResponse.contents[0].video.title}</h4>
+  const renderCards = () => {
+    const cards = [];
+
+    for (let i = 0; i < 5; i++) {
+      cards.push(
+        <div key={i} className="col-md-6">
+          <div
+            className="card text-center"
+            style={{
+              borderRadius: "2px",
+              background: "#C0C0C0",
+              boxShadow: "rgba(128, 128, 128, 0.5) 1px 1px 1px 1px",
+             
+            }}
+          >
+            <div className="card-img-top embed-responsive embed-responsive-16by9" >
+              <iframe
+                title={youtubeResponse.contents[i].video.title}
+                className="embed-responsive-item"
+                src={`https://www.youtube.com/embed/${youtubeResponse.contents[i].video.id}`}
+                allowFullScreen
+                width={"100%"}
+              ></iframe>
+            </div>
+            <div className="card-body">
+              <h4 className="card-title">
+                {youtubeResponse.contents[i].video.title}
+              </h4>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+    }
+
+    return cards;
+  };
+
+  return <div className="row">{renderCards()}</div>;
 };
 
 export default TeamHighlights;
