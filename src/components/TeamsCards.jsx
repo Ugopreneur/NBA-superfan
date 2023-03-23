7b0e439ca22bbb4aeeeafe6bd88c0048d979ea5a
mister-stan <lucianstan87@yahoo.com>
2023-03-23 05:24:29 +0000

    add the links between the functions

--- src/components/TeamsCards.jsx
+++ src/components/TeamsCards.jsx
@@ -3,13 +3,15 @@ import axios from "axios";
 
 const TeamsCard = (props) => {
     return (
-<div className="card" onClick={async () => {const options = {
-      method: 'GET',
-      url: 'https://api-nba-v1.p.rapidapi.com/teams',
-      params: {name: "Chicago Bulls"},
-      headers: {
-        'X-RapidAPI-Key': 'd73fe9001amshc84a879808281a3p118f3ejsnc92bedc355cd',
-        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    <div className="card" onClick={async () => {
+     
+      const options = {
+        method: 'GET',
+        url: 'https://api-nba-v1.p.rapidapi.com/teams',
+        params: {name: props.name },
+        headers: {
+          'X-RapidAPI-Key': 'd73fe9001amshc84a879808281a3p118f3ejsnc92bedc355cd',
+          'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
       }
     };
     
